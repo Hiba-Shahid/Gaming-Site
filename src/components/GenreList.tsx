02e54524bd674dcb31fa75a299f549ec89620bf6
Hlab2 <hibashahid678@gmail.com>
2025-01-25 02:51:25 +0500

    Refactor genre list component and remove unused data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,15 @@
-import { Heading, List, ListItem } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
-import { HStack } from "@chakra-ui/react";
-import { Image } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
-import { Text } from "@chakra-ui/react";
-import { Button, Spinner } from "@chakra-ui/react";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -17,33 +22,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (error) return null;
 
   if (isLoading) return <Spinner />;
+
   return (
     <>
-    <Heading fontSize='2xl' marginBottom={3} >Genres</Heading>
-    <List>
-      {data?.results.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              objectFit="cover"
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              whiteSpace="normal"
-              textAlign="left"
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-              variant="link"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+      <Heading fontSize="2xl" marginTop={9} marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="md"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
-import genres from "../data/genres";
-import apiClient from "../services/api-client";
-import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "./useData";
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import genres from '../data/genres';
+import { FetchResponse } from '../services/api-client';
 
 export interface Genre {
   id: number;
@@ -13,8 +13,10 @@ const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
     queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1000, //24h
+      apiClient
+        .get<FetchResponse<Genre>>('/genres')
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
     initialData: { count: genres.length, results: genres }
   });
 
