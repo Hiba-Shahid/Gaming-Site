235f5de81fcaa786e8a4b2f90a895599fb3e6fac
Hlab2 <hibashahid678@gmail.com>
2025-01-26 00:36:43 +0500

    Refactor API client to use a class-based approach and simplify data fetching in hooks

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import APIClient, {
+  FetchResponse,
+} from '../services/api-client';
+
+
+const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
   id: number;
@@ -17,8 +21,7 @@ const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
+      apiClient.getAll({
           params: {
             genres: gameQuery.genre?.id,
             parent_platforms: gameQuery.platform?.id,
@@ -26,7 +29,6 @@ const useGames = (gameQuery: GameQuery) =>
             search: gameQuery.searchText,
           },
         })
-        .then((res) => res.data),
   });
 
 export default useGames;
